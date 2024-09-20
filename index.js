02f3ec91cb3fa764557313999de93ed64a4446bb
Alutta <Aluttamatambo5@gmail.com>
2024-09-20 11:39:47 +0200

    created if and return

--- index.js
+++ index.js
@@ -37,5 +37,16 @@
 let bankBranchInstance = null;
 
 class BankBranch {
-    
+
+constructor(branchInfo) {
+    if (bankBranchInstance === null) {
+        this.branchInfo = branchInfo;
+        bankBranchInstance = this;
+}
+return bankBranchInstance;
+}
+
+getBranchInfo(){
+    return this.branchInfo;
+}
 }
